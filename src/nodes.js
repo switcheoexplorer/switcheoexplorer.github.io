e28fbabc99bc8913ec8cf581f6738ad059c11dcd
switcheoexplorer <44403666+switcheoexplorer@users.noreply.github.com>
2018-10-23 23:13:49 +0600

    Add files via upload

--- src/nodes.js
+++ src/nodes.js
@@ -1,12 +1,13 @@
 
 // set value gauge
-
+	bestblocks();
+	setInterval(bestblocks, 3000);
 	node1();
 	node2();
 	node3();
 	node4();
 	node5();
-	setInterval(setgauge, 3000);
+	setInterval(setgauge, 3500);
 
 // set gauge 2	
 function setgauge () {
@@ -14,11 +15,12 @@ function setgauge () {
 	var avgb = [vm.diff1, vm.diff2, vm.diff3, vm.diff4, vm.diff5];
 	var avgt = [vm.counter1, vm.counter2, vm.counter3, vm.counter4, vm.counter5]
 	var avg = [];	
+	document.getElementById('test').innerText = avgb;
 				var sum = 0;
 				for (var i = 0; i < 5; ++i) {	
-					avg[i] = (avgb[i] - avgt[i]/30)/2;
+					avg[i] = (avgb[i] - avgt[i]/30);
 					if (avg[i] < -3) avg[i] =  avg[i]*2; 
-					if (avg[i] == null || avg[i] < -120) avg[i] = -120;
+					if (avg[i] < -120) avg[i] = -120;
 					sum += avg[i];
 				};				
 				var res = 120+sum/5; 
@@ -26,8 +28,33 @@ function setgauge () {
 				
 				gauges[2].value = res;
 };
+ 
+//var urlb = "https://seed.o3node.org:10331"	
 
+function bestblocks () {
+
+	var urlb1 = "https://seed.o3node.org:10331";
+	var urlb2 = "https://seed1.neo.org:10331";
+	
+
+
+
+	var pollingPolicy = neo.service.createPollingPolicy(3000);
+	
+	neo.node(urlb1).poll(pollingPolicy).getBlockCount().notify(function (result) {
+		vm.bestb[0] = result; 
+	});
+	neo.node(urlb2).poll(pollingPolicy).getBlockCount().notify(function (result) {
+		vm.bestb[1] = result;
+	});
+	
+}
+
+
+<<<<<<< HEAD
+=======
 var urlb = "https://seed1.neo.org:10331"	
+>>>>>>> cb9fb1ad10a0d02fa5898bd755bf0cd1e4ae08ae
 
 // NODE 1
 async function node1 () {
@@ -49,27 +76,27 @@ async function node1 () {
         var pollingPolicy = neo.service.createPollingPolicy(3000);
 		
         vm.counter1 = ago;
-		var dif = 1;
+		
         
 		setInterval((function () {				
             document.getElementById('count1').innerText =  (++vm.counter1) + " s ago"; 				
 		}),1000);
 			
+
             neo.node(url).poll(pollingPolicy).getBlockCount().notify(function (result) {
 				if (height != result-1) {
 					vm.counter1 = -1; height = result-1; 
-				};				
+				};
+				
+				vm.diff1 = height - Math.max.apply(null, vm.bestb) + 1;
+				document.getElementById('height1').innerText =  height + ' (' + vm.diff1 + ')';
+				if (vm.diff1 < -3 || vm.counter1 > 120) {document.getElementById('nod1').style.color = "orange"} 
+				else {document.getElementById('nod1').style.color = "limegreen"}
+				
+
+				
           	});
-				  
-			neo.node(urlb).poll(pollingPolicy).getBlockCount().notify(function (result) {
-				 best = result; vm.diff1 = height - best + 1;
-				 if (vm.diff1 != dif) {
-					dif = vm.diff1;
-					document.getElementById('height1').innerText =  height + ' (' + vm.diff1 + ')';
-					if (vm.diff1 < -3 || vm.counter1 > 120) {document.getElementById('nod1').style.color = "orange"} 
-					else {document.getElementById('nod1').style.color = "limegreen"}
-				 }
-          	});	  
+			
 			
 			neo.node(url).poll(pollingPolicy).getVersion().notify(function (result) {
 				var ver = result.useragent;
@@ -108,7 +135,7 @@ async function node2 () {
         var pollingPolicy = neo.service.createPollingPolicy(3000);
 
         vm.counter2 = ago;
-		var dif = 1;
+		
 			
         setInterval((function () {	
              document.getElementById('count2').innerText = (++vm.counter2) + " s ago"; 
@@ -118,17 +145,14 @@ async function node2 () {
 				if (height != result-1) {
 					vm.counter2 = -1; height = result-1; 
 				};
-          	});
-				    
-			neo.node(urlb).poll(pollingPolicy).getBlockCount().notify(function (result) {
-				best = result; vm.diff2 = height - best+1; 
-				if (vm.diff2 != dif) {
-					dif = vm.diff2;
-					document.getElementById('height2').innerText = height + ' (' + vm.diff2 + ')';
+				vm.diff2 = height - Math.max.apply(null, vm.bestb) + 1;
+				document.getElementById('height2').innerText = height + ' (' + vm.diff2 + ')';
 					if (vm.diff2 < -3 || vm.counter2 > 120) {document.getElementById('nod2').style.color = "orange"} 
 					else {document.getElementById('nod2').style.color = "limegreen"}
-				}
+				
           	});
+				    
+			
 				  
 			neo.node(url).poll(pollingPolicy).getVersion().notify(function (result) {
 				var ver = result.useragent;
@@ -167,7 +191,7 @@ async function node3 () {
         var pollingPolicy = neo.service.createPollingPolicy(3000);
 
         vm.counter3 = ago;
-		var dif = 1;
+		
 			
         setInterval((function () {	
              document.getElementById('count3').innerText = (++vm.counter3) + " s ago"; 
@@ -177,18 +201,14 @@ async function node3 () {
 				if (height != result-1) {
 					vm.counter3 = -1; height = result-1; 
 				};
+				vm.diff3 = height - Math.max.apply(null, vm.bestb) + 1;
+				document.getElementById('height3').innerText = height + ' (' + vm.diff3 + ')';
+				if (vm.diff3 < -3 || vm.counter3 > 120) {document.getElementById('nod3').style.color = "orange"} 
+				else {document.getElementById('nod3').style.color = "limegreen"}
+				
           	});
 				   
-			neo.node(urlb).poll(pollingPolicy).getBlockCount().notify(function (result) {
-				best = result; vm.diff3 = height - best+1; 
-				if (vm.diff3 != dif) {
-					dif = vm.diff3;
-					document.getElementById('height3').innerText = height + ' (' + vm.diff3 + ')';
-					if (vm.diff3 < -3 || vm.counter3 > 120) {document.getElementById('nod3').style.color = "orange"} 
-					else {document.getElementById('nod3').style.color = "limegreen"}
-				}
-          	});
-				  
+			
 			neo.node(url).poll(pollingPolicy).getVersion().notify(function (result) {
 				var ver = result.useragent;
 				document.getElementById('ver3').innerText = ver;
@@ -226,7 +246,7 @@ async function node4 () {
         var pollingPolicy = neo.service.createPollingPolicy(3000);
 
         vm.counter4 = ago;
-		var dif = 1;
+		
 			
         setInterval((function () {	
              document.getElementById('count4').innerText = (++vm.counter4) + " s ago"; 
@@ -235,19 +255,14 @@ async function node4 () {
             neo.node(url).poll(pollingPolicy).getBlockCount().notify(function (result) {
 				if (height != result-1) {
 					vm.counter4 = -1; height = result-1; 
-				};				 
+				};	
+				vm.diff4 = height - Math.max.apply(null, vm.bestb) + 1;
+				document.getElementById('height4').innerText = height + ' (' + vm.diff4 + ')';
+					if (vm.diff4 < -3 || vm.counter4 > 120) {document.getElementById('nod4').style.color = "orange"} 
+					else {document.getElementById('nod4').style.color = "limegreen"}	
           	});
 				    
-			neo.node(urlb).poll(pollingPolicy).getBlockCount().notify(function (result) {
-				best = result; vm.diff4 = height - best+1; 
-				if (vm.diff4 != dif) {
-					dif = vm.diff4;
-					document.getElementById('height4').innerText = height + ' (' + vm.diff4 + ')';
-					if (vm.diff4 < -3 || vm.counter4 > 120) {document.getElementById('nod4').style.color = "orange"} 
-					else {document.getElementById('nod4').style.color = "limegreen"}				
-				}
-          	});				  
-
+			
 			neo.node(url).poll(pollingPolicy).getVersion().notify(function (result) {
 				var ver = result.useragent;
 				document.getElementById('ver4').innerText = ver;
@@ -285,7 +300,7 @@ async function node5 () {
         var pollingPolicy = neo.service.createPollingPolicy(3000);
 
         vm.counter5 = ago;
-		var dif = 1;
+		
 			
         setInterval((function () {	
              document.getElementById('count5').innerText = (++vm.counter5) + " s ago"; 
@@ -295,17 +310,10 @@ async function node5 () {
 				if (height != result-1) {
 					vm.counter5 = -1; height = result-1; 
 				};
-          	});
-				  
-  
-			neo.node(urlb).poll(pollingPolicy).getBlockCount().notify(function (result) {
-				best = result; vm.diff5 = height - best+1; 
-				if (vm.diff5 != dif) {
-					dif = vm.diff5;
-					document.getElementById('height5').innerText = height + ' (' + vm.diff5 + ')';
+				vm.diff5 = height - Math.max.apply(null, vm.bestb) + 1;
+				document.getElementById('height5').innerText = height + ' (' + vm.diff5 + ')';
 					if (vm.diff5 < -3 || vm.counter5 > 120) {document.getElementById('nod5').style.color = "orange"} 
-					else {document.getElementById('nod5').style.color = "limegreen"}				
-				}
+					else {document.getElementById('nod5').style.color = "limegreen"}
           	});
 				  
 
